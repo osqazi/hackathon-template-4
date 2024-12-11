a067c7fff575bbe5ab12c2b496cbab50ff9f3932
osqazi <107077552+osqazi@users.noreply.github.com>
2024-12-11 18:25:57 +0500

    Update shopItems.tsx

--- src/app/shopItems.tsx
+++ src/app/shopItems.tsx
@@ -1,79 +1,88 @@
-
 const shopItems = [
     {
-        "name":"Fusce pellentesque at",
-        "newprice":"26",
-        "oldprice":"42",
+        "id": 1,  // Numeric id
+        "name": "Fusce pellentesque at",
+        "newprice": "26",
+        "oldprice": "42",
         "pic": '/images/shopItems/Item1.png'
     },
     {
-        "name":"Ultricies condimentum imperdiet",
-        "newprice":"26",
-        "oldprice":"42",
-        "pic": '/images/shopItems/Item1.png'
-                
-    },
-    {
-        "name":"Vitae suspendisse sed",
-        "newprice":"26",
-        "oldprice":"42",
+        "id": 2,  // Numeric id
+        "name": "Ultricies condimentum imperdiet",
+        "newprice": "26",
+        "oldprice": "42",
         "pic": '/images/shopItems/Item1.png'
     },
     {
-        "name":"Sed at fermentum",
-        "newprice":"26",
-        "oldprice":"42",
+        "id": 3,  // Numeric id
+        "name": "Vitae suspendisse sed",
+        "newprice": "26",
+        "oldprice": "42",
         "pic": '/images/shopItems/Item1.png'
     },
     {
-        "name":"FFusce pellentesque at",
-        "newprice":"26",
-        "oldprice":"42",
+        "id": 4,  // Numeric id
+        "name": "Sed at fermentum",
+        "newprice": "26",
+        "oldprice": "42",
         "pic": '/images/shopItems/Item1.png'
     },
     {
-        "name":"Vestibulum magna laoreet",
-        "newprice":"26",
-        "oldprice":"42",
+        "id": 5,  // Numeric id
+        "name": "FFusce pellentesque at",
+        "newprice": "26",
+        "oldprice": "42",
         "pic": '/images/shopItems/Item1.png'
     },
     {
-        "name":"Sollicitudin amet orci",
-        "newprice":"26",
-        "oldprice":"42",
+        "id": 6,  // Numeric id
+        "name": "Vestibulum magna laoreet",
+        "newprice": "26",
+        "oldprice": "42",
         "pic": '/images/shopItems/Item1.png'
     },
     {
-        "name":"Ultrices mauris sit",
-        "newprice":"26",
-        "oldprice":"42",
+        "id": 7,  // Numeric id
+        "name": "Sollicitudin amet orci",
+        "newprice": "26",
+        "oldprice": "42",
         "pic": '/images/shopItems/Item1.png'
     },
     {
-        "name":"Pellentesque condimentum ac",
-        "newprice":"26",
-        "oldprice":"42",
+        "id": 8,  // Numeric id
+        "name": "Ultrices mauris sit",
+        "newprice": "26",
+        "oldprice": "42",
         "pic": '/images/shopItems/Item1.png'
     },
     {
-        "name":"Cras scelerisque velit",
-        "newprice":"26",
-        "oldprice":"42",
+        "id": 9,  // Numeric id
+        "name": "Pellentesque condimentum ac",
+        "newprice": "26",
+        "oldprice": "42",
         "pic": '/images/shopItems/Item1.png'
     },
     {
-        "name":"Lectus vulputate faucibus",
-        "newprice":"26",
-        "oldprice":"42",
+        "id": 10,  // Numeric id
+        "name": "Cras scelerisque velit",
+        "newprice": "26",
+        "oldprice": "42",
         "pic": '/images/shopItems/Item1.png'
     },
     {
-        "name":"Purus risus, ut",
-        "newprice":"26",
-        "oldprice":"42",
+        "id": 11,  // Numeric id
+        "name": "Lectus vulputate faucibus",
+        "newprice": "26",
+        "oldprice": "42",
         "pic": '/images/shopItems/Item1.png'
     },
-
-]
+    {
+        "id": 12,  // Numeric id
+        "name": "Purus risus, ut",
+        "newprice": "26",
+        "oldprice": "42",
+        "pic": '/images/shopItems/Item1.png'
+    }
+];
 
 export default shopItems;
