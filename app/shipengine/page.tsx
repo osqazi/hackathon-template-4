e096e81b0a291125619d734c1407bf49e7ee0865
osqazi <osqazi@gmail.com>
2025-02-04 13:15:34 +0500

    update

--- app/shipengine/page.tsx
+++
@@ -1,9 +0,0 @@
-import ShipEngine from "../components/ShipEngine";
-
-export default function ShipEnginePage() {
-  return (
-    <div className="container mx-auto p-4">
-      <ShipEngine />
-    </div>
-  );
-}
